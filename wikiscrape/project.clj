644b01a0740aed05fab54f7baad4c2c37b2bd763
govind <govindkrjoshi@gmail.com>
2016-07-21 01:28:07 +0530

    Add dependencies to project

--- wikiscrape/project.clj
+++ wikiscrape/project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.2.0"]
+                 [org.jsoup/jsoup "1.8.3"]])

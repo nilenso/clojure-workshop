a6d77721b6286ac07e48ce1f32d99d3d9a648342
govind <govindkrjoshi@gmail.com>
2016-07-21 17:09:24 +0530

    Remove unnecessary dependency

--- wikiscrape/project.clj
+++ wikiscrape/project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.2.0"]
                  [org.jsoup/jsoup "1.8.3"]])

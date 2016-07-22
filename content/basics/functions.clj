e05fbd15118191b993868d579e89458a487073ed
Srihari Sriraman <srihari@nilenso.com>
2016-07-22 19:08:58 +0530

    Add skeleton of material for functions.

---
+++ content/basics/functions.clj
@@ -0,0 +1,10 @@
+(ns content.basics.functions)
+
+;; syntax, defining and calling functions
+;; anonymous functions
+;; partial functions
+;; recursion
+;; higher order
+;; functional data type - set, map, etc
+;; composing functions - comp, juxt
+;; data > functions > macros

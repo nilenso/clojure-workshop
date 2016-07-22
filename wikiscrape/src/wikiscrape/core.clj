771a483664d75d7b00fdf5044d6179c76dab6d73
Srihari Sriraman <srihari@nilenso.com>
2016-07-22 18:47:43 +0530

    Minor changes to the scraper/crawler.

--- wikiscrape/src/wikiscrape/core.clj
+++ wikiscrape/src/wikiscrape/core.clj
@@ -11,11 +11,12 @@
   (.text e))
 
 (defn title [^Document doc]
+  #_ (-> doc (.select "#firstHeading") .text)
   #_(.. doc (select "#firstHeading") text)
   (.text (.select doc "#firstHeading")))
 
 (defn table-of-contents [^Document doc]
-  (.select doc "toc"))
+  (first (.select doc "#toc")))
 
 (defn description [^Document doc]
   (->> (.select doc "#mw-content-text")
@@ -26,33 +27,35 @@
        (map text)
        (s/join "\n")))
 
-(defn links-to-follow [^Document doc n]
+(defn links-to-follow [^Document doc num-links]
   (->> (.select doc "#mw-content-text a")
        (map #(.attr % "href"))
        (filter #(.startsWith % "/wiki"))
        (distinct)
-       (take n)
+       (take num-links)
        vec))
 
 (defn fetch-html [url]
   (.get (Jsoup/connect (str url-prefix url))))
 
-(defn info [^Document doc n]
+(defn info [^Document doc num-links]
   {:title (title doc)
    :table-of-contents (table-of-contents doc)
    :description (description doc)
-   :links-to-follow (links-to-follow doc n)})
+   :links-to-follow (links-to-follow doc num-links)})
 
 (defn write-page [{:keys [title] :as page}]
   (let [filename (s/replace title #"\s|/" "_")]
     (spit (str "scraped/" filename ".edn") (pr-str page))))
 
-(defn scrape [start-url n]
-  (let [page (info (fetch-html start-url) n)]
+(defn scrape [start-url depth num-links]
+  (let [page (info (fetch-html start-url) num-links)]
     (write-page page)
-    (when (pos? n)
-        (doseq [url (:links-to-follow page)]
-          (scrape url (dec n))))))
+    (when (pos? depth)
+      (doall
+       (map (fn [url]
+              (scrape url (dec depth) num-links))
+            (:links-to-follow page))))))
 
 (comment
-  (scrape "/wiki/Clojure" 1))
+  (scrape "/wiki/Clojure" 1 5))
